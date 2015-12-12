49c6fd7f1a2ac0a77dee9114e3ece8eeeb48385e
hgpt <paunalinconstantin@gmail.com>
2015-12-12 18:08:08 +0200

    Am facut tema4

---
+++ awj-base-project-master/angular-js-app/js/SmartTable.js
@@ -0,0 +1,8 @@
+$('#my-table').Tabledit({
+
+columns: {
+  identifier: [0, 'id'],                    
+  editable: [[1, 'First Name'], [2, 'Last Name']]
+}
+
+});
---
+++ awj-base-project-master/angular-js-app/js/appPersoana.js
@@ -0,0 +1,6 @@
+var app = angular.module('blog', [ ]);
+
+app.controller('PersoanaController', ['$scope', function($scope) {
+  $scope.Greeting = 'Controller Persoana';
+
+}]);
